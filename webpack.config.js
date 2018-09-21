9850c2262d989f90439e3ca0825a6ee7423fe1c1
Wellyngton Amaral <wellington.amaral.ufc@gmail.com>
2018-09-21 16:53:25 -0400

    Basic configuration webpack config

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './dist'
+  },
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			}
+		]
+	},
+	plugins: [
+    new HtmlWebpackPlugin({
+      template: './dist/index.html'
+    })
+  ],
+	output: {
+		publicPath: '/',
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
